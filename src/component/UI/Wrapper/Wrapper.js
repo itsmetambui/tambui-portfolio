a51dec48da2bba47aa86c91ce99d7d89744170b3
Tam Bui <itsmetambui@gmail.com>
2018-10-31 14:42:16 +0700

    implement wrapper component for presentation

---
+++ src/component/UI/Wrapper/Wrapper.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Wrapper.module.scss';
+
+const wrapper = props => {
+    return (
+        <div className={classes.Wrapper}>
+            <span>&nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+            <div>{props.children}</div>
+            <span>
+                &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+                <br />
+                &lt;/html&gt;
+            </span>
+        </div>
+    );
+};
+
+export default wrapper;
