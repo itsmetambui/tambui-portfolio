891e563f19c3370b824c6ac1d6b0f830b4139cee
Tam Bui <itsmetambui@gmail.com>
2018-10-31 18:53:55 +0700

    implement skills page

--- src/component/Skills/Skills.js
+++ src/component/Skills/Skills.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Wrapper from '../UI/Wrapper/Wrapper';
+import Header from '../UI/Header/Header';
+import classes from './Skills.module.scss';
+import TagCanvas from '../UI/TagCanvas/TagCanvas';
+import { Link } from 'react-router-dom';
 
 const skills = () => {
-    return <div>Skills</div>;
+    return (
+        <Wrapper>
+            <div className={classes.Skills}>
+                <div className={classes.TextArea}>
+                    <Header text="Skills / Experiences" />
+
+                    <p className={classes.Journey}>
+                        Main area of my expertise is front end development and everything related with web development.
+                        HTML, CSS, JS (ES5, ES6), building small & medium Web Apps, Angular, React.
+                    </p>
+                    <p className={classes.Journey}>
+                        Often I work as a full-stack developer - mainly in Java or Node with framework like Spring,
+                        Express...
+                    </p>
+
+                    <p className={classes.Journey}>
+                        I have also some experience working with databases like PostgreSQL, MongoBD or GraphQL.
+                    </p>
+
+                    <p className={classes.Journey}>
+                        Would like to know more?
+                        <br />
+                        Please just <Link to="/contact">contact</Link> me.
+                    </p>
+                </div>
+
+                <div className={classes.SkillChart}>
+                    <TagCanvas width="800" height="800" />
+                </div>
+            </div>
+        </Wrapper>
+    );
 };
 
 export default skills;
---
+++ src/component/UI/TagCanvas/TagCanvas.js
@@ -0,0 +1,88 @@
+/* eslint jsx-a11y/anchor-is-valid: 0 */
+import React, { Component } from 'react';
+import '../../../assets/js/tag-canvas.min';
+
+class TagCanvas extends Component {
+    componentDidMount() {
+        try {
+            window.TagCanvas.Start('skillChartCanvas', 'tags', options);
+        } catch (e) {
+            // something went wrong, hide the canvas container
+            console.log(e);
+            document.getElementById('skillChartContainer').style.display = 'none';
+        }
+    }
+    render() {
+        const tags = items.map(item => {
+            return (
+                <li>
+                    <a>{item}</a>
+                </li>
+            );
+        });
+        return (
+            <React.Fragment>
+                <div id="skillChartContainer">
+                    <canvas width={this.props.width} height={this.props.height} id="skillChartCanvas">
+                        <p>Tag Canvas</p>
+                    </canvas>
+                </div>
+                <div id="tags">
+                    <ul>{tags}</ul>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default TagCanvas;
+
+const options = {
+    textColour: '#ff7400',
+    outlineThickness: 0.5,
+    outlineColour: '#fe0853',
+    maxSpeed: 0.06,
+    freezeActive: true,
+    shuffleTags: true,
+    shape: 'sphere',
+    zoom: 1,
+    noSelect: true,
+    textFont: null,
+    pinchZoom: true,
+    freezeDecel: true,
+    fadeIn: 3000,
+    initial: [0.3, -0.1],
+    depth: 0.8
+};
+
+const items = [
+    'HTML 5',
+    'CSS 3',
+    'JavaScript',
+    'REST',
+    'AJAX',
+    'JSON',
+    'XML',
+    'Angular JS',
+    'Node JS',
+    'Express',
+    'Git',
+    'React',
+    'Bootstrap',
+    'SASS',
+    'RWD',
+    'Mongo DB',
+    'Karma',
+    'Jest',
+    'Gulp',
+    'npm',
+    'Bower',
+    'BEM',
+    'Canvas',
+    'Spring',
+    'Java',
+    'PostgreSQL',
+    'GraphQL',
+    'Jest',
+    'Material'
+];
